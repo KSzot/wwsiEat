f79c752bcf2367cbbc83e9f43e40b2f5e6368bc7
Krystian Szot <szot151@gmail.com>
2022-01-26 21:07:33 +0100

    added firebase auth and simple register form

--- wsEat/src/screens/RegisterScreen/RegisterScreen.tsx
+++ wsEat/src/screens/RegisterScreen/RegisterScreen.tsx
@@ -17,11 +17,32 @@ import {
   ScrollView,
 } from 'native-base';
 import {Header, FormRegister} from './components';
+import {Inputs} from './components/FormRegister';
 import {Platform} from 'react-native';
+import auth from '@react-native-firebase/auth';
 
 const RegisterScreen = ({navigation}: {navigation: any}) => {
   const onHandleSwitchToLoginScreen = () => navigation.navigate('Login');
-  const onHandleSubmit = () => console.log('Submit');
+  const onHandleSubmit = async (data: Inputs) => {
+    try {
+      const response = await auth().createUserWithEmailAndPassword(
+        data.email,
+        data.password,
+      );
+
+      await response.user.updateProfile({
+        displayName: data.name,
+      });
+    } catch (error: any) {
+      if (error?.code === 'auth/email-already-in-use') {
+        console.log('That email address is already in use!');
+      }
+
+      if (error?.code === 'auth/invalid-email') {
+        console.log('That email address is invalid!');
+      }
+    }
+  };
   return (
     <KeyboardAvoidingView
       flex="1"
