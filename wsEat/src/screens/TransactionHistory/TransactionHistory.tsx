df70dadedf26f796ae06440c9ffe6b90bfe23b87
Krystian Szot <szot151@gmail.com>
2022-02-28 22:30:00 +0100

    wip in transactions

--- wsEat/src/screens/TransactionHistory/TransactionHistory.tsx
+++ wsEat/src/screens/TransactionHistory/TransactionHistory.tsx
@@ -1,12 +1,31 @@
-import * as React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Box, Text, StatusBar} from 'native-base';
+import {SafeAreaView, View} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
+import firestore from '@react-native-firebase/firestore';
 
 const TransactionHistory = ({navigation}: {navigation: any}) => {
+  const currentUser = useSelector((store: RootStore) => store.User.user);
+  const [data, setData] = React.useState<any[] | undefined>(undefined);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await firestore()
+          .collection('Transaction')
+          .doc(currentUser.id)
+          .get();
+        setData(response?.data()?.transaction as any);
+      } catch (error) {
+        //wymaga obslugi
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
-    <Box>
+    <SafeAreaView style={{flex: 1, backgroundColor: 'lightBlue.50'}}>
       <StatusBar backgroundColor="#22c55e" />
-      <Text> TransactionHistory</Text>
-    </Box>
+    </SafeAreaView>
   );
 };
 
