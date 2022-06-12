9de9061602cbb407b74ef815a27dde80652ecab0
Krystian Szot <szot151@gmail.com>
2022-06-12 14:57:37 +0200

    added logout

--- wsEat/src/screens/Settings/Settings.tsx
+++ wsEat/src/screens/Settings/Settings.tsx
@@ -2,7 +2,29 @@ import * as React from 'react';
 import {Box, Text, StatusBar} from 'native-base';
 import {SafeAreaView} from 'react-native';
 import {MenuOptions, UserProfile} from './components';
+import auth from '@react-native-firebase/auth';
+import {CommonActions} from '@react-navigation/native';
 const Settings = ({navigation}: {navigation: any}) => {
+  const onHandleLogout = async () => {
+    await auth().signOut();
+    navigation.dispatch({
+      ...CommonActions.reset({
+        index: 0,
+        routes: [
+          {
+            name: 'Auth',
+            state: {
+              routes: [
+                {
+                  name: 'Login',
+                },
+              ],
+            },
+          },
+        ],
+      }),
+    });
+  };
   return (
     <SafeAreaView style={{flex: 1}}>
       <StatusBar backgroundColor="#22c55e" />
@@ -16,7 +38,7 @@ const Settings = ({navigation}: {navigation: any}) => {
         <MenuOptions
           iconName="logout"
           title="Wyloguj"
-          clicked={() => navigation.navigate('Password')}
+          clicked={onHandleLogout}
         />
       </Box>
     </SafeAreaView>
